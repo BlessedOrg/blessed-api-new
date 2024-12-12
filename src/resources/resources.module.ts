d5d9a14b832419dd3b102b106e61f5b5b243e1c4
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-12-12 11:34:57 +0100

    improve contracts endpoints performance

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -4,9 +4,11 @@ import { CampaignsModule } from "@/resources/campaigns/campaigns.module";
 import { ApplicationModule } from "@/resources/application/application.module";
 import { EventsModule } from "@/resources/events/events.module";
 import { TicketsModule } from "@/resources/tickets/tickets.module";
+import { DevelopersModule } from "@/resources/developers/developers.module";
+import { UsersModule } from "@/resources/users/users.module";
 
 @Module({
-  imports: [AudiencesModule, CampaignsModule, ApplicationModule, EventsModule, TicketsModule],
+  imports: [AudiencesModule, CampaignsModule, ApplicationModule, EventsModule, TicketsModule, DevelopersModule, UsersModule],
   controllers: [],
   providers: []
 })
